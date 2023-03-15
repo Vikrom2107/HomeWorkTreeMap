6d97ee864544383f211102fb31973e58c1679ca2
Viktor Romanov <vikrom2107@yandex.ru>
2023-03-15 20:23:16 +0700

    Branch Eraser

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,22 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> persons= new ArrayList<>();
-        persons.add(new Person("Vanya", "Pavlov", 25));
+        persons.add(new Person("Vanya", "Pavlov", 12));
         persons.add(new Person("Sveta", "Grenadina-Jukova", 18));
         persons.add(new Person("Sasha", "Barsukov", 38));
-        persons.add(new Person("Korney", "Lubovich Trumov", 44));
+        persons.add(new Person("Korney", "Lubovich Trumov", 15));
         persons.add(new Person("Tanya", "Zubina", 25));
         persons.add(new Person("Ctas", "Krot", 30));
         persons.add(new Person("Elena", "Jess Krown-Dabur", 27));
         System.out.println(persons);
-        
-        Comparator<Person> comparator = (o1, o2) -> {
-            int maxSurnameLength = 3;
-            String[] wordsInO1Surname = o1.getSurname().split("[-, ]");
-            int o1NumWords = wordsInO1Surname.length;
-            String[] wordsInO2Surname = o2.getSurname().split("[-, ]");
-            int o2NumWords = wordsInO2Surname.length;
 
-            if (o1NumWords < maxSurnameLength && o2NumWords < maxSurnameLength) {
-                if (Integer.compare(o2NumWords, o1NumWords) != 0) {
-                    return Integer.compare(o2NumWords, o1NumWords);
-                } else
-                    return Integer.compare(o2.getAge(), o1.getAge());
-            } else
-                return Integer.compare(o2.getAge(), o1.getAge());
-        };
-        Collections.sort(persons, comparator);
-        System.out.println("После сортировки 1 сортировки(кто знатнее(или старше) - тот выше): ");
+        Predicate<Person> under18 = p1 -> p1.getAge() < 18;
+
+        persons.removeIf(under18);
+
         System.out.println(persons);
     }
 }
